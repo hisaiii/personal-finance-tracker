111466db20cbe67123ca92d31ce1517411b06816
hisaiii <saiofficial0605@gmail.com>
2025-07-04 23:43:33 +0530

    hopes

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://personal-finance-tracker-8tdm.onrender.com/";
+export const BASE_URL = "https://personal-finance-tracker-8tdm.onrender.com";
 
 export const API_PATHS = {
   AUTH: {
